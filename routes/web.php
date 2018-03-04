c226e64467ee6f4b161f346b28aa546bbe6a25b7
Alex <1536678537@qq.com>
2018-03-04 20:59:55 +0800

    Add captcha

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,24 @@ Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm'
 Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+
+// [your site path]/Http/routes.php
+Route::any('captcha-test', function () {
+    if (Request::getMethod() == 'POST') {
+        $rules = ['captcha' => 'required|captcha'];
+        $validator = Validator::make(Input::all(), $rules);
+        if ($validator->fails()) {
+            echo '<p style="color: #ff0000;">Incorrect!</p>';
+        } else {
+            echo '<p style="color: #00ff30;">Matched :)</p>';
+        }
+    }
+    $form = '<form method="post" action="captcha-test">';
+    $form .= '<input type="hidden" name="_token" value="' .csrf_token() . '" >';
+    $form .= '<p>' . captcha_img() . '</p>';
+    $form .= '<p><input type="text" name="captcha"></p>';
+    $form .= '<p><button type="submit" name="check">Check</button></p>';
+        $form .= '</form>';
+        return $form;
+
+});
