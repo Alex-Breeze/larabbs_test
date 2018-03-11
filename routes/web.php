1e08b8bcce37a8f720b1410f3cc3f4496091deba
Alex <1536678537@qq.com>
2018-03-11 13:14:25 +0800

    Basic user profile editing

--- routes/web.php
+++ routes/web.php
@@ -23,13 +23,14 @@ Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
 Route::post('register', 'Auth\RegisterController@register');
 
-// Password Reset Routes
+// Password Reset Routes...
 Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
 Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
 Route::resource('users','UsersController', ['only' => ['show','update','edit']]);
+//Route::any('users','UsersController');
 
 
 
